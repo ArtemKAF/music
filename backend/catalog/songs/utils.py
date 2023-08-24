0b67ba928c38dacec7f77ea970fca62d86f6c1be
Kozin <grot_artem@inbox.ru>
2023-08-24 11:36:22 +0300

    Fixed parameter passing to formatted string

--- backend/catalog/songs/utils.py
+++ backend/catalog/songs/utils.py
@@ -2,10 +2,16 @@ from django.utils.translation import gettext_lazy as _
 
 
 def get_help_text_required_max_chars(max_chars):
-    return _('Required. %(max_chars)s characters or fewer.')
+    return _(
+        'Required. %(max_chars)s characters or fewer.'
+    ) % {'max_chars': max_chars}
 
 def get_error_message_min_validator(min_value):
-    return _('Value can not be less than %(min_value)s.')
+    return _(
+        'Value can not be less than %(min_value)s.'
+    ) % {'min_value': min_value}
 
 def get_error_message_max_validator(max_value):
-    return _('Value can not be more than %(max_value)s.')
+    return _(
+        'Value can not be more than %(max_value)s.'
+    ) % {'max_value': max_value}
