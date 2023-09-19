0a1ea73596bad08c0a1cfb7ce3d5c32cb7cf70c1
Kozin <grot_artem@inbox.ru>
2023-09-19 14:10:11 +0300

    Added stub file with type annotation

---
+++ backend/catalog/songs/api/serializers.pyi
@@ -0,0 +1,27 @@
+from typing import OrderedDict, Union
+
+from catalog.songs.models import Album, Singer
+
+class AlbumData(OrderedDict):
+    name: str
+    singer: Singer
+    year: int
+    songs: list[OrderedDict[str, Union[str, int]]]
+
+def add_songs(
+            self,
+            instance: Album,
+            songs: list[OrderedDict[str, Union[str, int]]]
+        ) -> None: ...
+
+def create(self, validated_data: AlbumData) -> Album: ...
+
+def update(
+            self,
+            instance: Album,
+            validated_data: AlbumData
+        ) -> Album: ...
+
+def to_representation(self, instance: Album) -> OrderedDict: ...
+
+def validate(self, data: AlbumData) -> AlbumData: ...
